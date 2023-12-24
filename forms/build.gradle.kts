8f1738f3b6d9f403cfa7660a01c7de3bec3c62c5
Chukwuemeka Nwagu <developer.nwagu@gmail.com>
2023-12-24 00:56:24 -0400

    add publishing config to build.gradle.kts

--- forms/build.gradle.kts
+++ forms/build.gradle.kts
@@ -1,10 +1,16 @@
 plugins {
     kotlin("multiplatform")
     id("com.android.library")
+    id("org.jetbrains.dokka")
+    id("maven-publish")
+    id("signing")
 }
 
 kotlin {
-    androidTarget()
+
+    androidTarget {
+        publishLibraryVariants("release", "debug")
+    }
 
     listOf(
             iosX64(),
@@ -45,15 +51,82 @@ task("testClasses").doLast {
     println("This is a dummy testClasses task")
 }
 
-//ext {
-//    publishGroupId = 'com.nwagu.forms'
-//    publishArtifactId = 'forms'
-//    publishVersion = '1.0.3'
-//
-//    libraryName = 'forms'
-//    libraryDescription = 'Kotlin multiplatform library for form management and validation'
-//}
+val dokkaOutputDir = "${layout.buildDirectory}/dokka"
 
-//if (project.rootProject.file('local.properties').exists()) {
-//    apply from: "${rootProject.projectDir}/publish-mavencentral.gradle"
-//}
+tasks.dokkaHtml {
+    outputDirectory.set(file(dokkaOutputDir))
+}
+
+val deleteDokkaOutputDir by tasks.register<Delete>("deleteDokkaOutputDirectory") {
+    delete(dokkaOutputDir)
+}
+
+val javadocJar = tasks.register<Jar>("javadocJar") {
+    dependsOn(deleteDokkaOutputDir, tasks.dokkaHtml)
+    archiveClassifier.set("javadoc")
+    from(dokkaOutputDir)
+}
+
+// begin region
+// Fix Gradle warning about signing tasks using publishing task outputs without explicit dependencies
+// https://github.com/gradle/gradle/issues/26091
+tasks.withType<AbstractPublishToMaven>().configureEach {
+    val signingTasks = tasks.withType<Sign>()
+    mustRunAfter(signingTasks)
+}
+tasks.withType<Sign>().configureEach {
+    val bundlingTasks = tasks.withType<AbstractArchiveTask>()
+    mustRunAfter(bundlingTasks)
+}
+// end region
+
+publishing {
+    repositories {
+        maven {
+            name = "OSSRH"
+            setUrl { "https://oss.sonatype.org/service/local/staging/deploy/maven2/" }
+            credentials {
+                username = System.getenv("OSSRH_USERNAME")
+                password = System.getenv("OSSRH_PASSWORD")
+            }
+        }
+    }
+    publications {
+        withType<MavenPublication> {
+            groupId = "com.nwagu.forms"
+            artifactId = "forms"
+            version = "2.0.0-alpha08"
+
+            artifact(javadocJar)
+
+            pom {
+                name = "forms"
+                description = "Kotlin multiplatform library for form management and validation"
+                url = "https://github.com/nwagu/forms"
+
+                licenses {
+                    license {
+                        name = "MIT License"
+                        url = "https://opensource.org/licenses/MIT"
+                    }
+                }
+                developers {
+                    developer {
+                        name = "Chukwuemeka Nwagu"
+                        email = "developer.nwagu@gmail.com"
+                    }
+                }
+                scm {
+                    connection = "scm:git:github.com/nwagu/forms.git"
+                    developerConnection = "scm:git:ssh://github.com/nwagu/forms.git"
+                    url = "https://github.com/nwagu/forms/tree/master"
+                }
+            }
+        }
+    }
+
+}
+
+signing {
+    sign(publishing.publications)
+}
